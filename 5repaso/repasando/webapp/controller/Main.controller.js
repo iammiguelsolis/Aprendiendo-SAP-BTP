48f6a0f683043d490f9fe47d68e726d7de67ca25
iammiguelsolis <miguelsoliscunza24@gmail.com>
2026-01-16 18:44:33 -0500

    conexiones

--- 5repaso/repasando/webapp/controller/Main.controller.js
+++ 5repaso/repasando/webapp/controller/Main.controller.js
@@ -7,6 +7,38 @@ sap.ui.define([
     return Controller.extend("uppersap.com.repasando.controller.Main", {
         onInit() {
             MessageToast.show('Hellow World!')
+        },
+
+        alPresionar: function () {
+            let oBoton = this.getView().byId('miBotonSupremo')
+            console.log(oBoton)
+            MessageToast.show(oBoton.getText())
+
+            oBoton.setText('Me has pulsado')
+
+            oBoton.setType('Accept')
+        },
+
+        onEscribiendo: function(oEvent) {
+            let oInput = oEvent.getSource()
+
+            if ( 'Miguel' === oInput.getValue() ) {
+                oInput.setEnabled(false)
+                MessageToast.show('Desactivado')
+            }
+        },
+
+        onValidar: function(oEvent) {
+            let oBoton = oEvent.getSource()
+
+            if ( 'Alonso' === this.getView().byId('inputUsuario').getValue()) {
+                oBoton.setType('Accept')
+                MessageToast.show('Bienvenido')
+            } else {
+                oBoton.setType('Reject')
+                MessageToast.show('Denegado')
+            }
         }
+
     });
 });
