d1ab3396db0cc69a8bf5a25e6e1f69e3668baff4
iammiguelsolis <miguelsoliscunza24@gmail.com>
2026-01-16 23:18:47 -0500

    probando el enlazamientos de Models

--- 5repaso/repasando/webapp/controller/Main.controller.js
+++ 5repaso/repasando/webapp/controller/Main.controller.js
@@ -1,11 +1,25 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
     "use strict";
 
     return Controller.extend("uppersap.com.repasando.controller.Main", {
         onInit() {
+
+            let oDatos = {
+                usuario: {
+                    nombre: "",
+                    rol: "Admin",
+                    activo: true
+                }
+            };
+
+            let oModelo = new JSONModel(oDatos)
+
+            this.getView().setModel(oModelo);
+
             MessageToast.show('Hellow World!')
         },
 
@@ -37,6 +51,18 @@ sap.ui.define([
             } else {
                 oBoton.setType('Reject')
                 MessageToast.show('Denegado')
+            }
+        },
+
+        onVerificarModelo: function() {
+            let oModelo = this.getView().getModel();
+
+            let sNombre = oModelo.getProperty("/usuario/nombre");
+
+            if (sNombre === "Alonso") {
+                MessageToast.show("¡Eres tú!");
+            } else {
+                MessageToast.show("Nombre incorrecto: " + sNombre);
             }
         }
 
